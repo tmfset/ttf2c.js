a7af6f6aaa30d6e578d7f5a939a4940925707ceb
Tom Fiset <tomj.fiset@gmail.com>
2022-02-20 15:17:56 -0500

    More cleanup

--- generate.js
+++ generate.js
@@ -1,24 +1,25 @@
 
 const indent = by => level => line => "".padStart(by * level, " ") + line;
-const indentDefault = indent(2)
+const defaultIndent = indent(2)
 
-const comment = line => "// " + line
+const singleLineComment = line => "// " + line
+const multiLineComment = lines => ["/**", ...(lines.map(l => " * " + l)), " */"]
 
 const toCodePointHex = c => "U+" + c.toString(16).padStart(4, "0");
 const toByteString = byte => "0x" + byte.toString(16).padStart(2, "0");
 const toByteDisplay = byte => byte > 0 ? "█" : " ";
 
-const glyphDeclName = glyph => `glyph_${glyph.codePoint}`;
+const toGlyphDeclarationName = glyph => `glyph_${glyph.codePoint}`;
 
 const toPixelDataRow = row => row.map(toByteString).join(", ");
 const toPixelDisplayRow = row => row.map(toByteDisplay).join("");
 
 const toPixelBlock = indentLevel => rows => {
-  const indent = indentDefault(indentLevel);
+  const indent = defaultIndent(indentLevel);
   const data = rows.map(toPixelDataRow).map(indent).join(",\n").split("\n");
-  const comments = rows.map(toPixelDisplayRow).map(comment);
+  const comments = rows.map(toPixelDisplayRow).map(singleLineComment);
   const lines = data.map((d, i) => d.padEnd(40, " ") + comments.slice(i, i + 1));
-  return lines.join("\n");
+  return lines;
 }
 
 const box = minWidth => lines => {
@@ -30,25 +31,29 @@ const box = minWidth => lines => {
   const middle = lines.map(l => "| " + l.padEnd(width - 4, " ") + "|");
   return [end, ...middle, end];
 }
+const defaultBox = box(40)
 
-const glyphDeclComment = glyph => {
+const toGlyphDeclarationComment = glyph => {
   const codePoint = toCodePointHex(glyph.codePoint).padEnd(8, " ")
   const name = `"${glyph.name}"`.padEnd(15, " ")
   const size = `${glyph.width}x${glyph.height}`.padEnd(5, " ")
   const offset = `(${glyph.xOffset}, ${glyph.yOffset})`
 
   const lines = [`${codePoint}${name}${size}${offset}`];
-  return box(40)(lines).map(comment).join("\n");
+  return multiLineComment(defaultBox(lines));
 }
 
-const glyphDecl = glyph => {
-  return `
-${glyphDeclComment(glyph)}
-static const uint8_t ${glyphDeclName(glyph)}[] = {
-${toPixelBlock(1)(glyph.pixels)}
-};`;
+const toGlyphDeclaration = glyph => {
+  return [
+    ...toGlyphDeclarationComment(glyph),
+    `static const uint8_t ${toGlyphDeclarationName(glyph)}[] = {`,
+    ...(toPixelBlock(1)(glyph.pixels)),
+    "};"
+  ]
 }
 
 export default function (glyphs) {
-  return glyphs.map(glyphDecl).join("\n");
+  return glyphs.map(glyph => {
+    return toGlyphDeclaration(glyph).join("\n");
+  }).join("\n");
 }
